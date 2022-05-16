6346ce0282c7d27180f3092b79dc59cd9c7aad5f
sergey211 <sergey211@gmail.com>
2022-05-16 20:18:53 +0300

    Add files via upload

--- task7.py
+++ task7.py
@@ -1,17 +1,20 @@
 import time
 counter=1
+found = False
 print(time.strftime("%H:%M:%S", time.localtime()))
 for i in range (2,100000000):
-    for x in range(2,i):
-        if i%x==0:
-            break
-        else:
-            if x==i-1:
-                counter=counter+1
-                if counter%1000==0:
-                    print(time.strftime("%H:%M:%S", time.localtime()))
-                    print(str(counter)+'='+str(i))
-                    time.sleep(1)
-                if counter==10001:
-                    print('iskomoe chislo='+str(i))
-                    break
+    if not found:
+        for x in range(2,i):
+            if i%x==0:
+                break
+            else:
+                if x==i-1:
+                    counter=counter+1
+                    if counter%1000==0:
+                        print(time.strftime("%H:%M:%S", time.localtime()))
+                        print(str(counter)+'='+str(i))
+                        time.sleep(1)
+                    if counter==10001:
+                        print('iskomoe chislo='+str(i))
+                        found=True
+                        break
