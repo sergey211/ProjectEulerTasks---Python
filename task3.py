338ad562d3779ce5189e52826a899af1b717c2f9
Sergey <sergey211@gmail.com>
2024-05-13 16:08:02 +0200

    task 3, 7 updated

--- task3.py
+++ task3.py
@@ -1,35 +1,36 @@
-#task 3 - новое решение
+# task 3 - новое решение
 import time
-t = time.localtime() 
-current_time = time.strftime("%H:%M:%S", t) 
-print('time: '+current_time)
-#x=13195
-x=600851475143 # stalo 53 sec
+
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+print('time: ' + current_time)
+# x=13195
+x = 600851475143  # stalo 53 sec
 global found
 found = False
 global chislo
 chislo = 0
-for i in range(2,x):
+for i in range(2, x):
     if found:
         break
     else:
-        if x%i == 0:
-            y=x/i
-            for j in range (2,x+1):
+        if x % i == 0:
+            y = x / i
+            for j in range(2, x + 1):
                 if found:
                     break
                 else:
-                    if y%j==0: 
-                        for l in range (2,x):
-                            if y%l==0:
+                    if y % j == 0:
+                        for l in range(2, x):
+                            if y % l == 0:
                                 break
-                            if l==y-1:
-                                chislo=y
+                            if l == y - 1:
+                                chislo = y
                                 # print('chislo = '+str(chislo)) 
-                                found = True                                
+                                found = True
                                 break
-                        break  
-print('iskomoe chislo = '+str(chislo))  
-t = time.localtime() 
-current_time = time.strftime("%H:%M:%S", t) 
-print('time: '+current_time)              
+                        break
+print('iskomoe chislo = ' + str(chislo))
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+print('time: ' + current_time)
--- task7.py
+++ task7.py
@@ -1,20 +1,21 @@
 import time
-counter=1
+
+counter = 1
 found = False
 print(time.strftime("%H:%M:%S", time.localtime()))
-for i in range (2,100000000):
+for i in range(2, 100000000):
     if not found:
-        for x in range(2,i):
-            if i%x==0:
+        for x in range(2, i):
+            if i % x == 0:
                 break
             else:
-                if x==i-1:
-                    counter=counter+1
-                    if counter%1000==0:
+                if x == i - 1:
+                    counter = counter + 1
+                    if counter % 1000 == 0:
                         print(time.strftime("%H:%M:%S", time.localtime()))
-                        print(str(counter)+'='+str(i))
+                        print(str(counter) + '=' + str(i))
                         time.sleep(1)
-                    if counter==10001:
-                        print('iskomoe chislo='+str(i))
-                        found=True
+                    if counter == 10001:
+                        print('iskomoe chislo=' + str(i))
+                        found = True
                         break
